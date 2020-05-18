ae36c425a17447639847ef8c13eb45250092fab1
LewLewonics <lewlewonics@gmail.com>
2020-05-17 19:44:00 -0700

    working on list

--- shows.js
+++ shows.js
@@ -17,5 +17,68 @@ let shows = [
         image:"images/shows/Yesterday-wo-Uttate.jpg",
         status:"watching",
         initial_display:""
-    }
+    },
+
+    {
+        title:"Another",
+        episodes:13,
+        current_episode: 13,
+        image:"images/shows/another.jpg",
+        status:"finished",
+        initial_display:"inactive"
+    },
+
+    {
+        title:"White Albumn 2",
+        episodes:13,
+        current_episode: 13,
+        image:"images/shows/white_albumn_2.jpg",
+        status:"finished",
+        initial_display:"inactive"
+    },
+
+    {
+        title:"Chihayafuru 3",
+        episodes:24,
+        current_episode: 24,
+        image:"images/shows/chihayafuru.jpg",
+        status:"finished",
+        initial_display:"inactive"
+    },
+
+    {
+        title:"Dennou Coil",
+        episodes:24,
+        current_episode: 12,
+        image:"images/shows/dennou_coil.jpg",
+        status:"watching",
+        initial_display:""
+    },
+
+    {
+        title:"Dennou Coil",
+        episodes:24,
+        current_episode: 12,
+        image:"images/shows/dennou_coil.jpg",
+        status:"watching",
+        initial_display:""
+    },
+
+    {
+        title:"My Next Life as a Villainess",
+        episodes:12,
+        current_episode: 7,
+        image:"images/shows/otome.jpg",
+        status:"watching",
+        initial_display:""
+    },
+
+    {
+        title:"Higurashi No Naku Koro Ni Kai",
+        episodes:26,
+        current_episode: 26,
+        image:"images/shows/higurash_kai.jpg",
+        status:"finished",
+        initial_display:"inactive"
+    },
 ];

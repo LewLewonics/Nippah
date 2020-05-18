9f3923b1383df4264d4fa927e826e2fbd8f8621e
LewLewonics <lewlewonics@gmail.com>
2020-05-17 19:24:51 -0700

    wip added show images and code

---
+++ shows.js
@@ -0,0 +1,21 @@
+var car = {type:"Fiat", model:"500", color:"white"};
+
+let shows = [
+    {
+        title:"Higurashi No Naku Koro Ni",
+        episodes:26,
+        current_episode: 26,
+        image:"images/shows/higurashi_no_naku_koro_ni.jpg",
+        status:"finished",
+        initial_display:"inactive"
+    },
+    
+    {
+        title:"Yesterday wo Uttate",
+        episodes:18,
+        current_episode: 7,
+        image:"images/shows/Yesterday-wo-Uttate.jpg",
+        status:"watching",
+        initial_display:""
+    }
+];

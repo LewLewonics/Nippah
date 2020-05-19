2236e8640a1b16d3dc701a014247c27ede15a2ac
LewLewonics <lewlewonics@gmail.com>
2020-05-19 03:02:57 -0700

    nipah activated

--- js/nipah.js
+++ js/nipah.js
@@ -1,22 +1,33 @@
-let image_container = document.getElementById("images-container");
-let num_of_rikas = 10;
+let image_container = document.getElementById("images_container");
+const num_of_rikas = 20;
 let rika_images = [];
 
-function create_array(num_of_rikas) {
-    
+function create_array() {
+    for (let i=0; i<num_of_rikas; i++) {
+        rika_images.push(`nipah${1}.jpg`);
+    }
 }
 
 function change_images() {
     for (let i=0; i<rika_images.length; i++) {
         image_container.innerHTML += `
-        <div class="cover-area">
             <div class="image-container">
-                <img src="rika${i+1}.jpg" alt="" class="anime-cover">
+                <img src="images/nipah${1}.jpg" alt="" class="nipah-img">
             </div>
-        </div>
         `;
     }
 }
 
+function change_color() {
+    let rikas = document.querySelectorAll('.nipah-img');
+    rikas.forEach((rika)=> {
+        let opacity = Math.random();
+        rika.style.opacity = `${opacity}`;
+    })
+}
 
 
+create_array();
+change_images();
+
+window.setInterval(change_color, 500);
